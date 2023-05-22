b30305a38c4fcd2a357974a05d468c3fcf2bcbee
1 <[regu7828@gmail.com]>
2023-05-22 16:42:41 +0900

    最新

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-h={1:3,3:4}
-print(h[0])
+import string
+text="Hey! What's up bro?,jibii"
+text=text.replace('!',' ')
+text=text.replace('?',' ')
+text=text.replace(',','')
+test_text=text.translate(str.maketrans('','',string.punctuation))
+text=test_text.split()
+print(text)
